f6679bbc411bd63c4e50b203d3d697f7a8e6505f
Charles Engelke <git@engelke.com>
2015-03-26 13:02:05 -0400

    Naive way to get around Apple prefixed implementation

--- digitalsign.js
+++ digitalsign.js
@@ -11,6 +11,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     "use strict";
 
+    // Fix Apple prefix if needed
+    if (window.crypto && !window.crypto.subtle && window.crypto.webkitSubtle) {
+        window.crypto.subtle = window.crypto.webkitSubtle;  // Won't work if subtle already exists
+    }
+
     if (!window.crypto || !window.crypto.subtle) {
         alert("Your current browser does not support the Web Cryptography API! This page will not work.");
         return;
